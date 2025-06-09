cef46f1e349769743f6f60332e5a6a928f1e4891
wanvoegbe sylvain <wanvoegbesylvain04@gmail.com>
2025-06-09 23:38:10 +0100

    Ajoute .env au .gitignore

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -164,6 +164,22 @@
                         </li>
                     </ul>
                 </li>
+
+                 <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="mdi mdi-label"></i>
+                        <span class="hide-menu">Inscriptions</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="{{ route('admin.users.activate') }}" class="sidebar-link">
+                                <i class="mdi mdi-plus-box"></i>
+                                <span class="hide-menu">Gérer les inscriptions </span>
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
